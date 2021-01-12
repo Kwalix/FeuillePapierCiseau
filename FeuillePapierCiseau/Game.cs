0e5bad1cea68f8e693488f36abfca90b3c2ce671
Kiwi <hallerlucas@outlook.com>
2021-01-12 16:52:44 +0100

    Ajoutez des fichiers projet.

---
+++ FeuillePapierCiseau/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeuillePapierCiseau
+{
+    class Game
+    {
+        private void NewGame(string userName)
+        {
+
+        }
+    }
+}
---
+++ FeuillePapierCiseau/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FeuillePapierCiseau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans Feuille Papier Ciseau !");
+
+        }
+    }
+}
